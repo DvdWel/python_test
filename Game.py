2bdf3d4abfa5b7c40387fd8016c961fba690765d
Danny Wel <dvdwel@hotmail.nl>
2019-07-08 10:48:13 +0200

    made the game OOP

--- Game.py
+++ Game.py
@@ -1,28 +1,61 @@
 #!/usr/bin/env python3
 import random
+import re
 
 class Game:
 
 	solution = ""
+	turn = 0
+	solved = False
+	opperators = "O?X"
 
 	def makeSolution(self):
-		solution = ""
+		solutionItem = ""
 		for i in range(4):
 			randNumber = str(random.randrange(0, 10))
 			unique = False
 			while unique == False:
 				counter = 0
-				for x in range(0, len(solution)):
-					if solution[x] == randNumber:
+				for x in range(0, len(solutionItem)):
+					if solutionItem[x] == randNumber:
 						randNumber = str(random.randrange(0, 10))
 					else:
 						counter += 1
-					if counter == len(solution):
+					if counter == len(solutionItem):
 						unique = True
-				if len(solution) == 0:
+				if len(solutionItem) == 0:
 					unique = True
-			solution += randNumber
-		self.solution = solution
+			solutionItem += randNumber
+		self.solution = solutionItem
+
+	def checkAttempt(self, attempt, solution):
+		if re.search('(?<!\\d)(?!0000)\\d{4}(?!\\d)', attempt) and attempt != "q":
+			if attempt == solution:
+				self.solved = True
+			else:
+				process = ""
+				compared = ""
+				for i in range(0, 4):
+					match = False
+					for x in range(0, 4):
+						if attempt[i] == solution[x]:
+							if attempt[i] == solution[i]:
+								compared += "O"
+								match = True
+							else:
+								compared += "?"
+								match = True
+					if match == False:
+						compared += "X"
+				for p in range(0,3):
+					for t in range(0,4):
+						if compared[t] == self.opperators[p]:
+							process += self.opperators[p]
+				print(process)
+			self.turn += 1
+		else:
+			if attempt != "q":
+				print("code is not 4 digits")
 
 	def getSolution(self):
 		return self.solution
--- play.py
+++ play.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 import random
-import re
 from Player import Player
 from Game import Game
 
 rules = '''O = Correct position
 ? = In code but wrong position
 X = Not in code'''
-opperators = "O?X"
 
 game = Game()
 player = Player()
@@ -16,42 +14,17 @@ random.seed(a=None, version=2)
 solution = ""
 print(rules)
 game.makeSolution()
-print(game.getSolution())
+solution = game.getSolution()
+print(solution)
 turn = 0
 solved = False
 attempt = ""
-while solved == False and attempt != "q":
+while game.solved == False and attempt != "q":
 	player.makeGuess()
 	attempt = player.getGuess()
-	if re.search('(?<!\\d)(?!0000)\\d{4}(?!\\d)', attempt) and attempt != "q":
-		if attempt == solution:
-			solved = True
-		else:
-			process = ""
-			compared = ""
-			for i in range(0, 4):
-				match = False
-				for x in range(0, 4):
-					if attempt[i] == solution[x]:
-						if attempt[i] == solution[i]:
-							compared += "O"
-							match = True
-						else:
-							compared += "?"
-							match = True
-				if match == False:
-					compared += "X"
-			for p in range(0,3):
-				for t in range(0,4):
-					if compared[t] == opperators[p]:
-						process += opperators[p]
-			print(process)
-		turn += 1
-	else:
-		if attempt != "q":
-			print("code is not 4 digits")
-if solved == True:
+	game.checkAttempt(attempt, solution)
+if game.solved == True:
 	print("cool")
-	print("it took you " + str(turn) + " turns")
+	print("it took you " + str(game.turn) + " turns")
 else:
-	print("you gave up after " + str(turn) + " turns")
+	print("you gave up after " + str(game.turn) + " turns")
